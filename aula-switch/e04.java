7d73ea0abfaa4b1038bceb05990bb5a226674bc9
Matheus K <matheusklauck8@gmail.com>
2025-08-27 18:54:08 -0300

    exericcios metodos

---
+++ aula-switch/e04.java
@@ -0,0 +1,26 @@
+
+/**
+ * Implemente um programa que leia o saldo médio de uma conta corrente.
+ * A seguir, o programa deve apresentar um menu de opções, semelhante a
+ * um caixa eletrônico: 1 - Sacar 2 - Depositar 3 - Exibir limite
+ * Para cada opção o programa deve ser comportar de uma forma diferente:
+ *      • Opção 1: solicita ao usuário o valor a ser sacado, se o valor for válido e
+ * o
+ * usuário puder sacar (considerar o saldo e o limite), o programa efetua o
+ * saque e exibe o saldo médio atual.
+ *      • Opção 2: solicita ao usuário o valor a ser depositado, se o valor for
+ * válido,
+ * o programa efetua o depósito e exibe o saldo médio atual.
+ *      • Opção 3: exibe o limite conforme abaixo
+ * Saldo menor que R$ 500,00 não há limite
+ * Saldo de R$ 500,00 a R$ 1.000,00 - 8% do saldo médio
+ * Saldo maior ou igual a R$ 1.000,00 - 15% do saldo médio
+ * 
+ */
+import java.util.Scanner;
+
+public class e04 {
+    public static void main(String args[]) {
+
+    }
+}
