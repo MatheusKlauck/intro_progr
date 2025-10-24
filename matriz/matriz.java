6332da3ca0c9435228c3d48c39ca12d7d8ff0970
Matheus K <matheusklauck8@gmail.com>
2025-10-24 18:56:34 -0300

    Matrix

---
+++ matriz/matriz.java
@@ -0,0 +1,86 @@
+
+/**
+ * Write a description of class matriz here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.*;
+import java.lang.reflect.Method;
+
+
+public class matriz
+{   
+    public static void ExibirMatriz(int [][] m) {
+        for( int i = 0; i < m.length; i++) {
+            for(int j = 0; j < m[0].length; j++) {
+                System.out.print(m[i][j] + "|");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    
+    public static int [][] PreencherMatriz(int [][]m, Random r) {
+        for( int i = 0; i < m.length; i++) {
+            for(int j = 0; j < m[0].length; j++) {
+                m[i][j] = r.nextInt(99);
+            }
+        }
+        return m;
+    }
+    
+    public static int SomarElementos(int[][] m) {
+        int soma = 0;
+        for( int i = 0; i < m.length; i++) {
+            for(int j = 0; j < m[0].length; j++) {
+                soma += m[i][j];
+            }
+        }
+        return soma;
+    }
+    
+    public static int[] DiagonalPrincipal(int[][] m ) {
+        int[] elementos = new int[m[0].length];
+        for( int i = 0; i < m.length; i++) {
+            for(int j = 0; j < m[0].length; j++) {
+                if(i == j) elementos[j] = m[i][j];
+            }
+        }
+        
+        return elementos;
+    }
+    
+    public static int[] DiagonalSecundaria(int[][]m) {
+        int[] elementos = new int[m[0].length];
+        for( int i = m.length; i >=0; i--) {
+            for(int j = 0; j < m[0].length; j++) {
+                if(i + j == m.length-1) elementos[j] = m[i][j];
+            }
+        }
+        
+        return elementos;
+    }
+    
+    public static String ExArray(int[] v) {
+        String el = "[";
+        for(int i =0; i < v.length; i++)
+            el += v[i] + ", ";
+        return el += "]";
+    }
+    
+    public static void main (String [] args) {
+        int linhas = 5;
+        int colunas = 5;
+        int[][] mat = new int [linhas][colunas];
+        mat = PreencherMatriz(mat, new Random());
+        ExibirMatriz(mat);
+
+        int soma = SomarElementos(mat);
+        System.out.println("Soma: "+soma);
+        int[] dp = DiagonalPrincipal(mat);
+        System.out.println("Diagonal Principal: "+ ExArray(dp));
+        int[] ds = DiagonalSecundaria(mat);
+        System.out.println("Diagonal Secundaria: "+ ExArray(ds));
+    }
+}
